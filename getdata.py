1ec6c7d615712c538b2466fb00c1c49050aab8ce
Denis <denis@neutigers.com>
2021-12-02 17:01:23 +0100

    Dev

--- getdata.py
+++ getdata.py
@@ -5,12 +5,27 @@ import pprint
 import time
 from datetime import date, datetime, timedelta
 
-pp = pprint.PrettyPrinter(indent=2, width=80, sort_dicts=True, compact=False)
+class Unbuffered(object):
+    def __init__(self, stream):
+       self.stream = stream
+    def __enter__(self):
+        return self
+    def __exit__(self, type, value, traceback):
+        pass
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+    def writelines(self, datas):
+        self.stream.writelines(datas)
+        self.stream.flush()
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
 
 DateFormat = "%Y-%m-%d"
-PDL_ID = '25212011521042'
 conn = http.client.HTTPSConnection("enedisgateway.tech")
+
 types = [
+    "addresses",
     "consumption_load_curve",
     "daily_consumption_max_power",
     "daily_consumption", 
@@ -37,27 +52,83 @@ headers = {
 def run(args):
     headers['Authorization'] = args.auth
     payload['usage_point_id'] = args.pdlid
-    payload['start'] = args.start_date.strftime(DateFormat)
-    payload['end'] = args.end_date.strftime(DateFormat)
+    start = args.start_date
+    end = args.end_date
+    intervals = []
+    if start > end:
+        _ = end
+        end = start
+        start = _
+    nIntervals, reminder = divmod((end-start).days, 7)
+    currentStart = start
+    for _ in range(nIntervals):
+        currentEnd = currentStart+timedelta(days=7)
+        intervals.append((currentStart.strftime('%Y-%m-%d'),
+                          currentEnd.strftime('%Y-%m-%d')))
+        currentStart += timedelta(days=7)
+    if reminder:
+        intervals.append(
+            (currentStart.strftime('%Y-%m-%d'),
+            (currentStart+timedelta(days=reminder)).strftime('%Y-%m-%d')))
     
-    for type in types:
-        payload['type'] = type
-        print(f'Acquisition de {type}')
-        pp.pprint(payload)
-        conn.request("POST", "/api", json.dumps(payload), headers)
-        res = conn.getresponse()
-        data = res.read()
-        data = json.loads(data.decode("utf-8"))
-        pp.pprint(data)
+    first1 = True
+    
+    rawOut = args.output
+    formattedOut = '.'.join((rawOut.split('.')[:-1])+['formatted.json'])
+    
+    with Unbuffered(open(rawOut, 'wt')) as out:
+        # pp = pprint.PrettyPrinter(indent=2, width=200, sort_dicts=True,
+        #                           compact=False, stream=out)
+        out.write('[')
+        for start, end in intervals:
+            payload['start'] = start
+            payload['end'] = end
+            if first1:
+                openBraket = '['
+                first1 = False
+            else:
+                openBraket = ',['
 
-        time.sleep(1)
-    
+            out.write(openBraket)
+            first2 = True
+            
+            for type in types:
+                if first2:
+                    openBraket = '['
+                    first2 = False
+                else:
+                    openBraket = ',['
+
+                out.write(openBraket)
+                payload['type'] = type
+                data = json.dumps(payload)
+                out.write(data)
+                out.write(',')
+                conn.request("POST", "/api", data, headers)
+                res = conn.getresponse()
+                data = res.read()
+                data = json.dumps(json.loads(data.decode("utf-8")))
+                out.write(data)
+                out.write(']')
+                out.flush()
+
+                time.sleep(1)
+
+            out.write(']')
+        out.write(']')
+
+    with open(rawOut, 'rt') as inFile:
+        with open(formattedOut, 'wt') as outFile:
+            json.dump(json.load(inFile), outFile, indent=2)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''
 ################
 Enedis collector
 ################
 ''')
+    maxDate = (datetime.now()-timedelta(days=(24*30.25)))
+    maxDateStr = maxDate.strftime('%Y-%m-%d')
     parser.add_argument('--auth', '-a', metavar='<AUTH>', type=str,
                         help='Authorization token, like mogHR8VzTLwrn...JFxlM '
                              'Get it on: https://enedisgateway.tech',
@@ -67,18 +138,26 @@ Enedis collector
                         'Get it on: '
                         'https://mon-compte-particulier.enedis.fr/compteur/',
                         required=True)
-    parser.add_argument('--start-date', '-s',
+    parser.add_argument('--start-date', '--start_date', '-s',
                         metavar='<START_DATE YYYY-MM-DD>',
                         type=date.fromisoformat, default=None,
-                        help='Start date (Default start date - 7 days)')
-    parser.add_argument('--end-date', '-e',
+                        help='Start date (Default = end-date - 7 days). '
+                             f'Must be > {maxDateStr}')
+    parser.add_argument('--end-date', '--end_date', '-e',
                         metavar='<END_DATE YYY-MM-DD>',
                         type=date.fromisoformat,
                         default=datetime.now()-timedelta(days=1),
                         help='End date (Default yesterday)')
+    parser.add_argument('--output', '-o',
+                        metavar='<output file>',
+                        type=str,
+                        default='enedis.json',
+                        help='Output file (Default to enedis.json)')
 
     args = parser.parse_args()
     if args.start_date is None:
         args.start_date = args.end_date - timedelta(days=7)
+    elif datetime.combine(args.start_date, datetime.min.time()) < maxDate:
+        args.start_date = maxDate
 
     run(args)
